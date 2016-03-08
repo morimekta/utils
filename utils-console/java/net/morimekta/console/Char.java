ca6313ab6cfff116ff8794a140bbe675a741eeb3
Stein Eldar Johnsen <morimekta@gmail.com>
2016-03-08 22:11:17 +0100

    Nit on char interface comments.

--- utils-console/java/net/morimekta/console/Char.java
+++ utils-console/java/net/morimekta/console/Char.java
@@ -1,19 +1,25 @@
 package net.morimekta.console;
 
 /**
- * General interface implemented by character-like classes.
+ * General interface implemented by character-like classes. A char
+ * can represent an actual character (Unicode), or a control sequence
+ * (Control, Color) that can alter the look or behaviour of the console
+ * the application runs in.
  */
 public interface Char {
     /**
      * Unicode codepoint representing this character.
      *
-     * @return The 32 but signed unicode codepoint, or -1 if not representing a
-     *         single unicode character.
+     * @return The 31 bit unsigned unicode code-point, or -1 if not
+     *         representing a single unicode character.
      */
     int codepoint();
 
     /**
-     * The number of spaces taken up by this character.
+     * The number of character spaces taken up by this symbol. Usually 1, but
+     * 0 for control sequences, control characters, and 2 for wide symbols like
+     * CJK characters.
+     *
      * @return The printable width of the character.
      */
     int printableWidth();
