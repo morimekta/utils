c593bad81bf5bb1e52f6fce3f8db7a66ea018a6c
Stein Eldar Johnsen <steineldar@zedge.net>
2016-05-10 10:32:59 +0200

    Add test for direct buffers.

--- io-util/src/test/java/net/morimekta/util/io/ByteBufferIOTest.java
+++ io-util/src/test/java/net/morimekta/util/io/ByteBufferIOTest.java
@@ -36,4 +36,29 @@ public class ByteBufferIOTest {
         assertArrayEquals(new byte[]{0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0},
                           indata2);
     }
+
+    @Test
+    public void testDirectBuffer() throws IOException {
+        byte[] data = new byte[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 4};
+
+        ByteBuffer buffer = ByteBuffer.allocateDirect(20);
+        buffer.limit(20);
+        ByteBufferOutputStream bbos = new ByteBufferOutputStream(buffer);
+
+        bbos.write(0);
+        bbos.write(data);
+        bbos.write(data, 6, 2);
+
+        buffer.flip();
+        ByteBufferInputStream bbis = new ByteBufferInputStream(buffer);
+
+        assertEquals(0, bbis.read());
+        byte[] indata = new byte[data.length];
+        assertEquals(16, bbis.read(indata));
+        assertArrayEquals(data, indata);
+        byte[] indata2 = new byte[data.length];
+        assertEquals(2, bbis.read(indata2, 6, 2));
+        assertArrayEquals(new byte[]{0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+                          indata2);
+    }
 }
