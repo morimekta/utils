ddde5a5c585b8f5ccaa07ea13b92242570b0c872
Stein Eldar Johnsen <morimekta@gmail.com>
2016-06-18 19:31:15 +0200

    Give the line printer some default formatted message printers.

--- console-util/src/main/java/net/morimekta/console/LinePrinter.java
+++ console-util/src/main/java/net/morimekta/console/LinePrinter.java
@@ -1,5 +1,7 @@
 package net.morimekta.console;
 
+import net.morimekta.console.chr.Color;
+
 /**
  * LinePrinter interface.
  */
@@ -9,4 +11,52 @@ public interface LinePrinter {
      * @param message The message to write.
      */
     void println(String message);
+
+    /**
+     * Print an info string message.
+     *
+     * @param message The info message.
+     */
+    default void info(String message) {
+        println(String.format("%s[info]%s %s",
+                              Color.GREEN,
+                              Color.CLEAR,
+                              message));
+    }
+
+    /**
+     * Print a warning string message.
+     *
+     * @param message The warning message.
+     */
+    default void warn(String message) {
+        println(String.format("%s[warn]%s %s",
+                              Color.YELLOW,
+                              Color.CLEAR,
+                              message));
+    }
+
+    /**
+     * Print an error string message.
+     *
+     * @param message The error message.
+     */
+    default void error(String message) {
+        println(String.format("%s[error]%s %s",
+                              Color.RED,
+                              Color.CLEAR,
+                              message));
+    }
+
+    /**
+     * Print a fatal string message.
+     *
+     * @param message The fatal message.
+     */
+    default void fatal(String message) {
+        println(String.format("%s[FATAL]%s %s",
+                              new Color(Color.RED, Color.BOLD),
+                              Color.CLEAR,
+                              message));
+    }
 }
