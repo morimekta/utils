4d2f0b255da2632a4f174b79e4cddf1ae696467a
Stein Eldar Johnsen <oss@morimekta.net>
2016-12-19 13:18:35 +0100

    BinaryReader methods overridden methods should be abstract.

--- io-util/src/main/java/net/morimekta/util/io/BinaryReader.java
+++ io-util/src/main/java/net/morimekta/util/io/BinaryReader.java
@@ -450,15 +450,9 @@ public abstract class BinaryReader extends InputStream {
 
     protected abstract int unshift2bytes(int b1, int b2);
 
-    protected int unshift3bytes(int b1, int b2, int b3) {
-        return (b1 | b2 << 8 | b3 << 16);
-    }
+    protected abstract int unshift3bytes(int b1, int b2, int b3);
 
-    protected int unshift4bytes(int b1, int b2, int b3, int b4) {
-        return (b1 | b2 << 8 | b3 << 16 | b4 << 24);
-    }
+    protected abstract int unshift4bytes(int b1, int b2, int b3, int b4);
 
-    protected long unshift8bytes(long b1, long b2, long b3, long b4, long b5, long b6, long b7, long b8) {
-        return (b1 | b2 << 8 | b3 << 16 | b4 << 24 | b5 << 32 | b6 << 40 | b7 << 48 | b8 << 56);
-    }
+    protected abstract long unshift8bytes(long b1, long b2, long b3, long b4, long b5, long b6, long b7, long b8);
 }
