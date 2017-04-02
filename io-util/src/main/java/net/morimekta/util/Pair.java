0c1f3ed9478de7e1a078b182e43ed6aab3337cd3
Stein Eldar Johnsen <oss@morimekta.net>
2017-04-02 13:12:22 +0200

    Add pair to io-util.
    It's been too many times I needed something like this...

---
+++ io-util/src/main/java/net/morimekta/util/Pair.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2016, Stein Eldar johnsen
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package net.morimekta.util;
+
+import java.util.Objects;
+
+/**
+ * Container to ease passing around a tuple of two objects. This object
+ * provides a sensible implementation of equals(), returning true if equals()
+ * is true on each of the contained objects.
+ */
+public final class Pair<F, S> {
+    public final F first;
+    public final S second;
+
+    /**
+     * Constructor for a Pair.
+     *
+     * @param first First part of pair.
+     * @param second Second part of pair.
+     */
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Convenience method for creating an appropriately typed pair.
+     *
+     * @param <A> First type.
+     * @param <B> Second type.
+     * @param a First part.
+     * @param b Second part.
+     * @return The resulting pair.
+     */
+    public static <A, B> Pair<A, B> create(A a, B b) {
+        return new Pair<>(a, b);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || !(getClass().equals(o.getClass()))) {
+            return false;
+        }
+        Pair<?, ?> other = (Pair<?, ?>) o;
+
+        return (Objects.equals(first, other.first) &&
+                Objects.equals(second, other.second));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Pair.class, first, second);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s,%s)",
+                             Objects.toString(first),
+                             Objects.toString(second));
+    }
+}
---
+++ io-util/src/test/java/net/morimekta/util/PairTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016, Stein Eldar johnsen
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package net.morimekta.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class PairTest {
+    @Test
+    public void testEquals() {
+        Pair<Integer, String> a = Pair.create(4, "a");
+        Pair<Integer, String> b1 = Pair.create(4, "b");
+        Pair<Integer, String> b2 = Pair.create(4, "b");
+
+        assertNotEquals(a, b1);
+        assertEquals(b1, b2);
+
+        assertTrue(a.equals(a));
+        assertFalse(a.equals(null));
+        assertFalse(a.equals(new Object()));
+    }
+
+    @Test
+    public void testHashCode() {
+        Pair<Integer, String> a = Pair.create(4, "a");
+        Pair<Integer, String> b1 = Pair.create(4, "b");
+        Pair<Integer, String> b2 = Pair.create(4, "b");
+        Pair<UUID, String> c1 = Pair.create(null, "c");
+        Pair<String, UUID> c2 = Pair.create("c", null);
+
+        assertNotEquals(a.hashCode(), b1.hashCode());
+        assertEquals(b1.hashCode(), b2.hashCode());
+        assertNotEquals(c1.hashCode(), c2.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        Pair<Integer, String> a = Pair.create(4, "a");
+        Pair<Integer, String> b = Pair.create(4, "b");
+        Pair<UUID, String> c = Pair.create(null, "c");
+
+        assertEquals("(4,a)", a.toString());
+        assertEquals("(4,b)", b.toString());
+        assertEquals("(null,c)", c.toString());
+    }
+}
