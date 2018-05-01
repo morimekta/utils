21168792219c34e1c4da05242766f3764d10dbd2
Stein Eldar Johnsen <oss@morimekta.net>
2018-05-01 10:43:15 +0200

    Javadoc trigger updates.

--- trigger-javadoc.sh
+++ trigger-javadoc.sh
@@ -3,12 +3,12 @@ VERSION=${1:-}
 
 if [[ -z "$1" ]]
 then
-  VERSION=$(git tag | grep '^v[0-9]' | sort -V | tail -n 1 | sed 's/^v//')
+  VERSION=$(git tag | grep '^v[0-9]\+[.][0-9]\+[.][0-9]\+' | sort -V | tail -n 1 | sed 's/^v//')
 fi
 
 for m in utils $(cat pom.xml | grep '<module>' | sed -e 's/.*<module>//' -e 's:</.*::' | sort)
 do
   URL="http://www.javadoc.io/doc/net.morimekta.utils/$m/$VERSION"
   echo -e "\033[01m$URL\033[00m"
-  curl -s $URL > /dev/null || exit 1
+  curl -s ${URL} > /dev/null || exit 1
 done
